3b38a632baa5594ff8c46bd6d03a446abbd0321d
Pawan Gambhir <122386120+AlienwareSec@users.noreply.github.com>
2024-06-07 18:02:32 +0530

    Add files via upload

---
+++ Three-tier-app/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-c3uci82cykud.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Admin0630',
+    DB_DATABASE : ''
+});
